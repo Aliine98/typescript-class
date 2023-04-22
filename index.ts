3548b1f62dcf7ba04c9e09d8c407899db032c941
Aline Bevilacqua <90913523+Aliine98@users.noreply.github.com>
2023-04-22 17:17:14 -0300

    ♻ refactor: add import statement for class Car

--- index.ts
+++ index.ts
@@ -1,17 +1,4 @@
-class Car {
-  brand: string;
-  color: string;
-  fuelExpense: number;
-  constructor(brand: string, color: string, kmPerL: number) {
-    this.brand = brand;
-    this.color = color;
-    this.fuelExpense = 1 / kmPerL;
-    console.log(`Carro da marca ${brand}, da cor ${color}, que gasta ${this.fuelExpense.toFixed(2)} litros de combustível por quilômetro`);
-  }
-  calcTripPrice(km: number, fuelPrice: number): string {
-    return (km * this.fuelExpense * fuelPrice).toFixed(2);
-  }
-}
+import { Car } from "./Car";
 
 const gol: Car = new Car('Volkswagen', 'prata', 10);
 console.log(`O preço da viagem é de R$ ${gol.calcTripPrice(100, 5)}`);
