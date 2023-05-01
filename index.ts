3bb54eb5a9027ee751697fd6ba97688c550d4833
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-05-01 03:18:33 -0300

    ✨ feat: use Person and calculateIMC

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { Car } from "./Car";
+import { Person } from "./Person";
 
 const gol: Car = new Car('Volkswagen', 'prata', 10);
 console.log(`O preço da viagem é de R$ ${gol.calcTripPrice(100, 5)}`);
 const uno: Car = new Car('Fiat', 'preto', 15);
 console.log(`O preço da viagem é de R$ ${uno.calcTripPrice(100, 5)}`);
+
+
+const jose = new Person('José', 70, 1.75);
+console.log("jose imc:", jose.calculateIMC());
